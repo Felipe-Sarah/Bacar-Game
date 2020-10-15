ad4ed2bb38cc9b055ec9c4ca4cea4b440023a625
felipeschiavinato <72338515+felipeschiavinato@users.noreply.github.com>
2020-10-15 20:31:00 -0300

    Update Bacará-Games.py

--- Bacará-Games.py
+++ Bacará-Games.py
@@ -43,17 +43,20 @@ while j <= i:
 
 print(players)
 
-# Seting Decks
-i = int(input('Number of decks '))
-deck = deck * i
+# Setting Decks
+n = int(input('Number of decks (1/6/8) '))
+while n != 1 and n != 6 and n != 8:
+    print('Invalid number of decks')
+    n = int(input('Number of decks (1/6/8) '))
+deck = deck * n
 
 
 # Seting Values
 
-
+points = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '1':0, 'J':0, 'Q':0, 'K':0}
 
 # Game Starts
-random.shuffle(deck)
+
 i = 0
 while i < len(players):
     print(players[i] + ' recieves 100 chips')
@@ -61,35 +64,133 @@ while i < len(players):
 
 chips = [100]*len(players)
 
-# Placing bets
 
-bets_chips = []
-bets_winners = []
-i = 0
-j = 1
-while i < len(players):
-    bets_chips.append(int(input('{} place your bet value '.format(players[i]))))
-    bets_winners.append(input('on whom? (Table, House or Tie) '))
-    i += 1
-    j += 1
+while True:
 
-#Card Value
-points = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':0, 'J':0, 'Q':0, 'K':0}
+    random.shuffle(deck)
 
-a = deck[0]
-b = deck[1]
-c = deck[2]
-d = deck[3]
+    # Placing bets
+
+    bets_chips = []
+    bets_winners = []
+    i = 0
+    j = 1
+    while i < len(players):
+        bets_chips.append(int(input('{} place your bet value '.format(players[i]))))
+        bets_winners.append(input('on whom? (Table, House or Tie) '))
+        i += 1
+        j += 1
+
+    table_cards = [deck[0], deck[1]]
+    house_cards = [deck[2], deck[3]]
+
+    print('Table has {}'.format(table_cards))
+    print('House has {}'.format(house_cards))
 
 
-table_points = points[a[0]] + points[b[0]]
-print(table_points)
-if table_points >= 10:
-    table_points = table_points - (table_points - (table_points % 10))
-print('Table has {} points'.format(table_points))
 
-house_points = points[c[0]] + points[d[0]]
-print(house_points)
-if house_points >= 10:
-    house_points = house_points - (house_points - (house_points % 10))
-print('House has {} points'.format(house_points))
+    a = deck[0]
+    b = deck[1]
+    c = deck[2]
+    d = deck[3]
+    e = deck[4]
+    f = deck[5]
+
+
+
+    table_points = points[a[0]] + points[b[0]]
+    if table_points >= 10:
+        table_points = table_points - (table_points - (table_points % 10))
+    print('Table has {} points'.format(table_points))
+
+    house_points = points[c[0]] + points[d[0]]
+    if house_points >= 10:
+        house_points = house_points - (house_points - (house_points % 10))
+    print('House has {} points'.format(house_points))
+
+
+    while True:
+
+        if table_points > 7 or house_points > 7:
+            if house_points == table_points:
+                print('Tie')
+                resultado = 'Tie'
+                break
+            elif table_points > house_points:
+                print('Table wins')
+                resultado = 'Table'
+                break
+            else:
+                print('House wins')
+                resultado = 'House'
+                break
+
+        if table_points < 6:
+            table_cards.append(e)
+            print('Table has {}'.format(table_cards))
+            table_points = table_points + points[e[0]]
+            if table_points >= 10:
+                table_points = table_points - (table_points - (table_points % 10))
+            print('Table has {} points'.format(table_points))
+
+        
+
+        if house_points < 6 and len(table_cards) == 2:
+            house_cards.append(e)
+            print('House has {}'.format(house_cards))
+            house_points = house_points + points[e[0]]
+            if house_points >= 10:
+                house_points = house_points - (house_points - (house_points % 10))
+            print('House has {} points'.format(house_points))
+
+        while house_points < 6 and len(table_cards) == 3:
+
+            if house_points == 5 and points[e[0]] < 4 and points[e[0]] > 7:
+                break
+            elif house_points == 4 and points[e[0]] < 2 and points[e[0]] > 7:
+                break    
+            elif house_points == 4 and points[e[0]] == 8:
+                break
+            else:
+                house_cards.append(f)
+                print('House has {}'.format(house_cards))
+                house_points = house_points + points[f[0]]
+                if house_points >= 10:
+                    house_points = house_points - (house_points - (house_points % 10))
+                print('House has {} points'.format(house_points))
+                break
+
+        if house_points == table_points:
+            print('Tie')
+            resultado = 'Tie'
+            break
+        elif table_points > house_points:
+            print('Table wins')
+            resultado = 'Table'
+            break
+        else:
+            print('House wins')
+            resultado = 'House'
+            break
+
+    # Pagamento de apostas
+    i = 0
+    while i < len(players):
+        if n == 1:
+            if resultado == 'Tie':
+                if bets_winners[i] == resultado:
+                    print('{} wins {} chips'.format(players[i], 8*bets_chips[i]*0.8425))
+                    chips[i] = chips[i] + 
+                
+
+
+    
+
+
+
+
+
+
+
+
+
